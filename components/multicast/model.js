e10039f7702ee3c76069036cb5026924f9b7b5fd
Mert Dogar <mertdogar@gmail.com>
2016-02-20 15:25:05 +0200

    Improve multicast reserve method

--- components/multicast/model.js
+++ components/multicast/model.js
@@ -2,13 +2,36 @@
 
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const ObjectId = mongoose.Types.ObjectId;
 
 
 const multicastSchema = new Schema({
     ip: {type: String, required: true, unique: true},
-    status: {type: Boolean, default: false},
+    active: {type: Boolean, default: false},
     project: {type: Schema.Types.ObjectId, ref: 'Project'}
 }, {timestamps: true});
+
+
+/**
+ * Reserves the next available ip address to a project.
+ * @param {ObjectId} projectId
+ * @returns {Promise}
+ */
+multicastSchema.statics.reserve = function(projectId) {
+    return this
+        .findOneAndUpdate(
+            {active: false},
+            {$set: {project: projectId, active: true}},
+            {new: true}
+        )
+        .exec()
+        .then(multicast => {
+            if (!multicast)
+                throw new Error('Could not find an available ip address');
+
+            return multicast.ip;
+        });
+}
 
 
 module.exports = mongoose.model('Multicast', multicastSchema);
