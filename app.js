3397af27a5bccfec200c53f693db861ed71dd663
Mert Dogar <mertdogar@gmail.com>
2016-02-20 20:34:15 +0200

    Add APIError and error middleware

--- app.js
+++ app.js
@@ -76,29 +76,6 @@ app.use(function(req, res, next) {
     next(err);
 });
 
-
-// error handlers
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.json({
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.json({
-        message: err.message,
-        error: {}
-    });
-});
-
+app.use(require('dovecote/lib/middlewares/error'));
 
 module.exports = app;
