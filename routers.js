6e5b84a683dcbc4abaf63118ddc51687f99bc7d9
Mert Dogar <mertdogar@gmail.com>
2016-02-20 11:07:17 +0200

    Implenent barebones of service model and controller

--- routers.js
+++ routers.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 
 router.use('/api/users', require('dovecote/components/user/router'));
+router.use('/api/services', require('dovecote/components/service/router'));
 router.use('/api/components', require('dovecote/components/component/router'));
 
 
