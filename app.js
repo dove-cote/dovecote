6ae906ff45244a3583a23cb874faca53707da3bf
Deniz Gurkaynak <dgurkaynak@gmail.com>
2016-02-20 10:58:21 +0200

    Initalize mongo connection

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+require('dovecote/lib/mongo');
 
 var app = express();
 
