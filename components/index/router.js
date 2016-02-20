0d51718d12dd76a13f1ccb5753a62b8219fc1acb
dashersw <armagan@amcalar.com>
2016-02-20 13:07:53 +0100

    Implement frontend builds on dashboard, and an index view

---
+++ components/index/router.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var auth = require('dovecote/lib/middlewares/auth');
+
+
+router.get('/', function(req, res) {
+    res.render('index');
+});
+
+
+router.get('/dashboard',
+    // auth.ensureAuthenticationOrRedirect, // TODO: comment in for authentication check
+    function(req, res) {
+    var bundle = 'bundle.js';
+
+    try {
+        JSON.parse(fs.readFileSync(__dirname + '/../../public/stats.json'));
+        bundle = stats.assetsByChunkName.main[0];
+    } catch(e) {}
+
+    res.render('dashboard', {
+        bundle: bundle
+    });
+});
+
+
+module.exports = router;
