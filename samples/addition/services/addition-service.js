d542dab903c8fbca1661e92230c727afd035e169
dashersw <armagan@amcalar.com>
2016-02-20 16:42:21 +0100

    Capitalize addition requester name

--- samples/addition/services/addition-service.js
+++ samples/addition/services/addition-service.js
@@ -1,7 +1,7 @@
 var cote = require('cote', {multicast: '239.1.1.1'});
 
 var additionResponder = new cote.Responder({
-    name: 'addition Responder',
+    name: 'Addition Responder',
     namespace: 'addition',
     respondsTo: ['add']
 });
