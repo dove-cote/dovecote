3c884294c5b3e4c34885c3c81ca36939d5a0bc20
Mert Dogar <mertdogar@gmail.com>
2016-02-21 13:44:16 +0200

    Add state to project model to indicate running status

--- components/project/model.js
+++ components/project/model.js
@@ -4,12 +4,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const deepPopulate = require('mongoose-deep-populate')(mongoose);
 
+const States = [
+    'running',
+    'terminated',
+    'error'
+];
 
 const projectSchema = new Schema({
     name: {type: String, required: true},
     services: [{type: Schema.Types.ObjectId, ref: 'Service'}],
     owner: {ref: 'User', required: true, type: Schema.Types.ObjectId},
     multicastIP_: String,
+    state: {type: String, enum: States, default: 'terminated'},
+    deployedServices: {type: Schema.Types.Mixed},
     lastDeploy: Date
 }, {timestamps: true});
 
