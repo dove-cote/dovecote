df1c6ed45e537ec0977cc50d0dc3d2dba04ac32b
Fatih Erikli <fatiherikli@gmail.com>
2016-02-21 21:06:57 +0200

    Set the link of logo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,42 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import ProjectView from './components/ProjectView';
+import {browserHistory} from 'react-router';
 
 import store from './store';
-
 import styles from './App.module.css';
+
 require('./App.less');
 require('./components/buttons.less');
 
 window.__STORE__ = store;
 
-
-
 var UserInfo = React.createClass({
-
     render() {
         var {email, username} = this.props.user;
-
-        return <div className="">
-            {email ? <div>logged in as: {username} <a href='/logout'>Logout</a></div>:<div>Not logged in <a href='/login'>Login</a>  </div>}
-        </div>;
-
+        return (
+          <div>
+            {username ? 
+              <div>{username} <a href='/logout'>Logout</a></div>
+            : <div>Not logged in <a href='/login'>Login</a></div>}
+          </div>
+        );
     }
-
 });
 
 
 var Header = React.createClass({
+    navigate(event) {
+        event.preventDefault();
+        browserHistory.push('/projects');
+    },
 
     render() {
         var user = store.getUser().toJS();
         return (
           <header className='top-header'>
-            <img src='/img/logo.png' className='logo' />
+            <a onClick={this.navigate} href='#'>
+               <img src='/img/logo.png' className='logo' />
+            </a>
             {user.initialized && (
               <div className='user-container'>
                 <UserInfo user={user} />
