ef25d3127a7cdfe4e8aa4e382364b79f72a5066a
Deniz Gurkaynak <dgurkaynak@gmail.com>
2016-02-21 17:02:51 +0200

    Fix wierd promise issue :metal:

--- scripts/deploy-all-projects.js
+++ scripts/deploy-all-projects.js
@@ -1,22 +1,24 @@
 require('dovecote/app');
 const Project = require('dovecote/components/project/model');
 const ProjectService = require('dovecote/components/project/service');
+const ProjectGenerator = require('dovecote/components/project/generator');
 
 Project
     .find({})
     .select('_id owner')
-    .exec((err, projects) => {
-        if (err)
-            return console.log('Cannot get projects', err);
+    .exec()
+    .then((projects) => {
+        console.log(`Deploying ${projects.length} project(s)...`);
 
         const jobs = projects.map(project => ProjectService.deploy(project._id, project.owner));
         return Promise.all(jobs);
     })
     .then(() => {
         console.log('Done!');
-        setTimeout(process.exit.bind(process), 1000);
+        process.exit();
     })
     .catch((err) => {
         console.log(err);
+        process.exit();
     });
 
