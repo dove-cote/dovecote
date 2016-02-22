e937652c262e41ead0e91c1d5435974d8bb07f88
dashersw <armagan@amcalar.com>
2016-02-22 03:11:15 +0100

    Fix typo in project generator

--- components/project/generator/service.js
+++ components/project/generator/service.js
@@ -16,7 +16,7 @@ class ServiceGenerator {
 
 
     run() {
-        if (this.data.name == 'Getaway') {
+        if (this.data.name == 'Gateway') {
             debug(`Ignoring to generate "Getaway" named service, skipping`);
             return Promise.resolve();
         }
