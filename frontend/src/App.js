84f17cbee1a4c302a3b1a2b76c22e610da94eab0
Ustun Ozgur <ustun@ustunozgur.com>
2016-02-22 07:55:04 +0200

    add intercom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,8 @@ import styles from './App.module.css';
 require('./App.less');
 require('./components/buttons.less');
 
+import $ from 'jquery';
+
 window.__STORE__ = store;
 
 var UserInfo = React.createClass({
@@ -52,6 +54,27 @@ var Header = React.createClass({
 
 });
 
+var intercomSnippet = `
+<script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/lmps7kt9';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
+`;
+
+
+var intercomSnippet2 = `
+<script>
+  window.intercomSettings = {
+    app_id: "ojq15o4y"
+  };
+</script>
+<script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/ojq15o4y';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
+`;
+
+
+function injectIntercom() {
+//    $('body').append($(intercomSnippet));
+
+    $('head').append($(intercomSnippet));
+}
+
 
 var App = React.createClass({
 
@@ -61,9 +84,13 @@ var App = React.createClass({
 		});
 	},
 
+    componentWillMount() {
+        injectIntercom();
+    },
 	componentDidMount() {
 		store.addListener(this.update);
         store.fetchUser();
+
 	},
 
 
